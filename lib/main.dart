f8ae5a8db181be66255ee016fc18480971ef6bac
ATS527 <truespiritz527@gmail.com>
2021-03-18 20:12:20 +0530

    All done....ready to deploygit add .git add .

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,7 @@ void main(List<String> args) {
   ));
 }
 
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/questionPage.dart
+++ lib/questionPage.dart
@@ -8,30 +8,56 @@ class QuestionPage extends StatefulWidget {
 
 class _QuestionPageState extends State<QuestionPage> {
   int i = 0;
+  int totalScore = 0;
+  String get resultText {
+    String result;
 
-  List questions = [
+    if (totalScore > 100)
+      result = 'You are Awesome....';
+    else if (totalScore > 50)
+      result = 'You are good enough';
+    else if (totalScore < 50) result = 'You are bad';
+    return result;
+  }
+
+  List questions = <Map>[
     {
       "questionText": "What s your favourite pet!",
-      "options": ["cheetah", "dog", "cat", "elephant"]
+      "answers": [
+        {"text": "cheetah", "score": 32},
+        {"text": "dog", "score": 20},
+        {"text": "cat", "score": 10},
+        {"text": "elephant", "score": 0}
+      ],
     },
     {
       "questionText": "What s your favourite food!",
-      "options": ["pizza", "fry", "chicken", "biriyani"]
+      "answers": [
+        {"text": "pizza", "score": 20},
+        {"text": "fry", "score": 10},
+        {"text": "chicken", "score": 40},
+        {"text": "biriyani", "score": 0}
+      ],
     },
     {
       "questionText": "What s your favourite sports!",
-      "options": ["cricket", "football", "hockey", "badminton"]
+      "answers": [
+        {"text": "cricket", "score": 5},
+        {"text": "football", "score": 40},
+        {"text": "hockey", "score": 20},
+        {"text": "badminton", "score": 0}
+      ],
     },
   ];
 
-  Widget optionButtons(List<String> options) {
+  Widget optionButtons(List<Map> options) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
         SizedBox(
           height: 20,
         ),
-        for (String option in options)
+        for (Map options in options)
           ElevatedButton(
               style: ButtonStyle(
                 backgroundColor: MaterialStateProperty.resolveWith<Color>(
@@ -44,23 +70,62 @@ class _QuestionPageState extends State<QuestionPage> {
               ),
               onPressed: () {
                 setState(() {
-                  if (i < questions.length - 1) {
-                    i++;
-                  }
+                  totalScore += options["score"];
+                  i++;
                 });
               },
-              child: Text(option))
+              child: Text(options["text"]))
       ],
     );
   }
 
+  void restartQuiz() {
+    i = 0;
+    totalScore = 0;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Question(questions[i]['questionText']),
-        optionButtons(questions[i]['options']),
-      ],
+    return Container(
+      child: i < questions.length
+          ? Column(
+              children: <Widget>[
+                Question(questions[i]['questionText']),
+                optionButtons(questions[i]['answers']),
+              ],
+            )
+          : Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Your Score is $totalScore !!!\n $resultText",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        restartQuiz();
+                      });
+                    },
+                    child: Text("Restart"),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                        (Set<MaterialState> states) {
+                          if (states.contains(MaterialState.pressed))
+                            return Colors.grey;
+                          return Colors.green; // Use the component's default.
+                        },
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
     );
   }
 }
